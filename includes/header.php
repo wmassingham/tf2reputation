c6ce086ae93e7c772ce80d114304c2e83f4717c0
Webster Massingham <wmassingham@gmail.com>
2014-06-03 17:15:04 -0400

    php merge

--- includes/header.php
+++ includes/header.php
@@ -1,4 +1,9 @@
-<?php session_start(); ?>
+<?php
+	session_start();
+
+	require_once('openid.php');
+	require_once('apis.php');
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -59,7 +64,27 @@
 									<li><a href="#">One more separated link</a></li>
 								</ul>
 							</li>-->
-							<?php include('includes/steamuser.php'); ?>
+							<?php
+								$openid = new LightOpenID('tf2reputation.com');
+
+								if($openid->validate() || isset($_SESSION['id'])) {
+									$userid = explode("/", $_SESSION['id'])[5];
+									$loggedinuserdata = new SteamAPI($userid);
+
+									// These are in reverse order because they are floated right
+									echo '<li class="navbar-right"><a style="padding:6px 15px 0 0;" href="profile.php?id=' .
+										$loggedinuserdata->data->response->players[0]->steamid . '"><img class="avatar-small ' .
+										$loggedinuserdata->onlineState . '" alt="Player avatar" src="' .
+										$loggedinuserdata->data->response->players[0]->avatarfull . '"></a>';
+									echo '<li class="navbar-right"><a href="profile.php?id=' .
+										$loggedinuserdata->data->response->players[0]->steamid . '">' .
+										$loggedinuserdata->data->response->players[0]->personaname . '</a></li>';
+								} else {
+									echo '<li class="navbar-right"><a href="login.php?login&amp;loc=' . urlencode($_SERVER['REQUEST_URI']) . 
+										'" style="padding:3px 15px 3px 0;"><img class="img-responsive" alt="Sign in through Steam" ' .
+										'src="http://cdn.steamcommunity.com/public/images/signinthroughsteam/sits_large_noborder.png"></a></li>';
+								}
+							?>
 						</ul>
 
 					</div><!--/.navbar-collapse -->
